a244eb8f8c61fd234308a808a0d58214cb6d9a18
Marcus "Loki" Jenz <jenz@silpion.de>
2021-03-18 19:40:13 +0100

    init

---
+++ src/components/MovieComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <figure class="figure"><img class="cover" v-bind:src="movie.cover"></figure>
+    <div class="card-body">
+      <div class="card-title" v-text="movie.title"></div>
+      <div class="card-text sub-title" v-text="movie.altTitle"></div>
+      <div class="card-text description" v-text="movie.description"></div>
+
+      <div class="tags">
+        <div  v-for="tag in movie.tags" class="badge bg-success tag" v-text="tag"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {Movie} from "../model/Movie";
+
+export default {
+  name: "MovieComponent",
+  props: {
+    movie: Movie
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
